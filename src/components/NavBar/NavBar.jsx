2198285b2f165e43be848c1586ab36407ffd2848
Abu Bakor Siddik (Shuvo) <141806136+Abs-shuvo@users.noreply.github.com>
2026-05-09 10:33:17 +0600

    responsive navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from './Link';
+import { Menu, X } from 'lucide-react';
 
 const navigationData = [
   {
@@ -30,8 +31,11 @@ const navigationData = [
 ];
 
 const NavBar = () => {
+    const [open,setOpen] = useState(false);
+    const links = navigationData.map(route => <Link key={route.id} route={route}></Link>)
+
     return (
-        <nav>
+        <nav className='flex justify-between mx-10'>
             {/* <ul className='flex gap-10'>
                 <li><a href="/">Home</a></li>
                 <li><a href="/about">About</a></li>
@@ -43,12 +47,19 @@ const NavBar = () => {
                     navigationData.map(route => <li className='mr-10'><a href={route.path}>{route.name}</a></li> )
                 }
             </ul> */}
-
-            <ul className='flex '>
+            <span className='flex' onClick={() =>setOpen(!open)}>
+                {open? <X className='md:hidden'></X> : <Menu className='md:hidden'></Menu>}
+                <ul className='md:hidden'>
+                    {links}
+                </ul>
+                <h3 className='ml-5'>My Navbar</h3>
+            </span>
+            <ul className='md:flex hidden'>
                 {
-                navigationData.map(route => <Link key={route.id} route={route}></Link>)
+                    links
                 }
             </ul>
+            <button>Sign In</button>
         </nav>
     );
 };
