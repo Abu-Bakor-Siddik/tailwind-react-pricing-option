63e301164b42ba2ee7178d2839da8be79b03f09e
Abu Bakor Siddik (Shuvo) <141806136+Abs-shuvo@users.noreply.github.com>
2026-05-09 10:46:28 +0600

    full navbar complete

--- src/components/NavBar/Link.jsx
+++ src/components/NavBar/Link.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Link = ({route}) => {
     return (
         <div>
-            <li className='mr-10'>
+            <li className='md:mr-10 px-4 py-1 hover:bg-amber-500'>
                 <a href={route.path}>{route.name}</a>
             </li>
         </div>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -35,7 +35,7 @@ const NavBar = () => {
     const links = navigationData.map(route => <Link key={route.id} route={route}></Link>)
 
     return (
-        <nav className='flex justify-between mx-10'>
+        <nav className='flex justify-between mx-10 mt-5'>
             {/* <ul className='flex gap-10'>
                 <li><a href="/">Home</a></li>
                 <li><a href="/about">About</a></li>
@@ -49,7 +49,7 @@ const NavBar = () => {
             </ul> */}
             <span className='flex' onClick={() =>setOpen(!open)}>
                 {open? <X className='md:hidden'></X> : <Menu className='md:hidden'></Menu>}
-                <ul className='md:hidden'>
+                <ul className={`md:hidden absolute duration-1000 ${open? 'top-12' : '-top-40'} bg-amber-100 rounded-2xl`}>
                     {links}
                 </ul>
                 <h3 className='ml-5'>My Navbar</h3>
